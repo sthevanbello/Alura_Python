1562aa7317410c70ca7922b9a6c9c9c78822f296
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-05-15 20:35:18 -0300

    Alura Course - Part 02

--- forca.py
+++ forca.py
@@ -1,13 +1,28 @@
 def jogar():
-    
 
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************\n")
 
-
-
-    print("End of game!")
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    lista = []
     
-if (__name__ == "__main__"):
+    while (not enforcou and not acertou):
+        chute = input("Digite uma letra: ").lower().strip()
+        
+        index = 0
+        
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print(f"Acertou a letra {chute} na posição {index+1}")
+                lista.insert(index, letra)
+            index += 1
+        print(lista)    
+            
+    print("End of game!")
+
+
+if __name__ == "__main__":
     jogar()
